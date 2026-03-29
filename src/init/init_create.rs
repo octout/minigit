f3e81ab67029fc5c1755d99777d0d9379f67f753
octout <gg.yuuto@gmail.com>
2026-03-29 12:54:59 +0900

    fix init behavior

--- src/init/init_create.rs
+++ src/init/init_create.rs
@@ -2,20 +2,18 @@ use std::fs;
 use std::fs::File;
 
 pub fn create_init_file() -> Result<(), String> {
-    fs::create_dir("./.minigit")
-        .map_err(|e| format!("failed to create .minigit: {}", e))?;
-    
-    File::create("./.minigit/index")
-        .map_err(|e| format!("failed to create index: {}", e))?;
-    
-    fs::create_dir("./.minigit/refs")
-        .map_err(|e| format!("failed to create refs: {}", e))?;
-    
-    File::create("./.minigit/refs/main")
-        .map_err(|e| format!("failed to create main: {}", e))?;
-    
-    fs::create_dir("./.minigit/objects")
-        .map_err(|e| format!("failed to create objects: {}", e))?;
-    
+    fs::create_dir("./.minigit").map_err(|e| format!("failed to create .minigit: {}", e))?;
+
+    File::create("./.minigit/HEAD").map_err(|e| format!("failed to create HEAD: {}", e))?;
+
+    fs::create_dir("./.minigit/refs").map_err(|e| format!("failed to create refs: {}", e))?;
+
+    fs::create_dir("./.minigit/refs/heads")
+        .map_err(|e| format!("failed to create haeds: {}", e))?;
+
+    File::create("./.minigit/refs/main").map_err(|e| format!("failed to create main: {}", e))?;
+
+    fs::create_dir("./.minigit/objects").map_err(|e| format!("failed to create objects: {}", e))?;
+
     Ok(())
 }
